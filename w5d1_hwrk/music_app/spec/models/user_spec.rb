6400e559c62191ca5252c543adfebb90356a6f0d
tjshiu <tiffany.shiu.2@gmail.com>
2018-03-12 07:02:32 -0700

    w5d1 hwrk

---
+++ w5d1_hwrk/music_app/spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe User do
+    subject(:user) do
+      FactoryBot.build(:user, email: "aj@aj.com", password: "password")
+    end
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+
+end
